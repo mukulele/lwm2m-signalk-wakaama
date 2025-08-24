8ac3d4142b3e0e42dad164c41c2451be17cc5603
Christian Kaiser <christian.kaiser@messagerie.de>
2025-08-24 20:59:58 +0200

    Add minimal SignalK WebSocket client using libwebsockets

--- examples/client/common/lwm2mclient.c
+++ examples/client/common/lwm2mclient.c
@@ -827,6 +827,110 @@ void print_usage(void) {
 #endif
     fprintf(stdout, "\r\n");
 }
+// START wakatiwai
+
+static char * server_get_uri(lwm2m_object_t * obj, uint16_t instanceId) {
+    int size = 1;
+    lwm2m_data_t * dataP = lwm2m_data_new(size);
+    dataP->id = 0; // security server uri
+    char * uriBuffer;
+
+    obj->readFunc(instanceId, &size, &dataP, obj);
+    if (dataP != NULL &&
+            (dataP->type == LWM2M_TYPE_STRING || dataP->type == LWM2M_TYPE_OPAQUE) &&
+            dataP->value.asBuffer.length > 0) {
+        uriBuffer = lwm2m_malloc(dataP->value.asBuffer.length + 1);
+        memset(uriBuffer, 0, dataP->value.asBuffer.length + 1);
+        strncpy(uriBuffer, (const char *) dataP->value.asBuffer.buffer, dataP->value.asBuffer.length);
+        lwm2m_data_free(size, dataP);
+        return uriBuffer;
+    }
+    lwm2m_data_free(size, dataP);
+    return NULL;
+
+}
+
+static uint16_t object_id_contains(uint16_t objectId, uint16_t * objectIdArray, uint16_t len) {
+    uint16_t result = 0;
+    uint16_t i = 0;
+    if (objectId < 1 || objectIdArray == NULL || len < 1) {
+        return result;
+    }
+    for (; i < len; i++) {
+        if (objectIdArray[i] == objectId) {
+            result = 1;
+            break;
+        }
+    }
+    return result;
+}
+
+static uint16_t * parse_object_id_csv(const char * objectIdCsv, uint16_t * objCount) {
+    uint16_t count = 1;
+    uint16_t buffIdx = 0;
+    uint16_t objectId = 0;
+    uint16_t objectIndex = 0;
+    uint16_t * objectIdArray;
+    char buff[12];
+    const char * c = objectIdCsv;
+    for (; *c != '\0'; c++) {
+        if (*c == ',') {
+            ++count;
+        }
+    }
+    c = objectIdCsv;
+    objectIdArray = lwm2m_malloc(sizeof(uint16_t) * count);
+    memset(objectIdArray, 0, sizeof(uint16_t) * count);
+    for (; *c != '\0'; c++) {
+        if (*c == ',') {
+            if (buffIdx > 11) {
+                fprintf(stderr, "Too long Object ID\r\n");
+                lwm2m_free(objectIdArray);
+                return NULL;
+            }
+            buff[buffIdx] = '\0';
+            objectId = strtol(buff, NULL, 10);
+            if (object_id_contains(objectId, objectIdArray, objectIndex)) {
+                // duplicate object ID, ignored.
+            } else  if (objectId > 3) {
+                objectIdArray[objectIndex++] = objectId;
+            } else {
+                fprintf(stderr, "Invalid Object ID\r\n");
+                lwm2m_free(objectIdArray);
+                return NULL;
+            }
+            buffIdx = 0;
+        } else {
+            buff[buffIdx++] = *c;
+        }
+    }
+    if (buffIdx > 11) {
+        fprintf(stderr, "Too long Object ID\r\n");
+        lwm2m_free(objectIdArray);
+        return NULL;
+    } else if (buffIdx > 0) {
+        buff[buffIdx] = '\0';
+        objectId = strtol(buff, NULL, 10);
+        if (objectId > 3) {
+            objectIdArray[objectIndex++] = objectId;
+        } else {
+            fprintf(stderr, "Invalid Object ID\r\n");
+            lwm2m_free(objectIdArray);
+            return NULL;
+        }
+    }
+    *objCount = objectIndex;
+#ifdef WITH_LOGS
+    uint16_t i = 0;
+    for (; i < objectIndex; i++) {
+      fprintf(stderr, ">>> %hu => ObjectID:[%hu] \r\n", i, objectIdArray[i]);
+    }
+    // 4 objects(/0,/1,/2,/3) are implicitly included
+    fprintf(stderr, ">>> %hu objects will be deployed as well as predfined 4 objects\r\n", *objCount);
+#endif
+    return objectIdArray;
+}
+// END wakatiwai
 
 int main(int argc, char *argv[]) {
     client_data_t data;
