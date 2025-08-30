675c88b3f60c5300222f0049ae6d0c51cab6f519
Christian Kaiser <christian.kaiser@messagerie.de>
2025-08-30 13:52:43 +0200

    chore: Remove build artifacts from version control
    - Remove build/ directory contents from git tracking
    - These files should be generated locally and not committed
    - Gitignore rules now properly exclude build artifacts
    - Aligns with wakaama project best practices
    
    Build artifacts include:
    - Compiled executables and libraries
    - CMake configuration files
    - Test scripts and configuration files
    - Custom configuration files
    
    These will be regenerated by the build.sh script as needed.

--- examples/signalk-lwm2m-client/udp/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/wakaama/examples/signalk-lwm2m-client/udp
-# Build directory: /home/pi/wakaama/examples/signalk-lwm2m-client/udp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("wakaama_lib")
